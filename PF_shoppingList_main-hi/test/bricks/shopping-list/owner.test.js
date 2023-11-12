57e9c782f3bfa5d98269ca9dd677dec3a61618af
Petr Falc <petr.falc@unicornuniversity.net>
2023-11-12 19:12:12 +0100

    SL Name Owner List added

---
+++ PF_shoppingList_main-hi/test/bricks/shopping-list/owner.test.js
@@ -0,0 +1,14 @@
+import PFShoppingList from "PF_shoppingList_main-hi";
+import { mount, wait } from "uu5g05-test";
+
+describe(`PFShoppingList.Bricks.ShoppingList.Owner`, () => {
+  // it("onLoad", async () => {
+  //   let onLoad = jest.fn(async () => ({ content: "Hello, world!" }));
+  //   let loadDtoIn = { foo: "bar" };
+  //   let wrapper = mount(<PFShoppingList.Bricks.ShoppingList.Owner onLoad={onLoad} loadDtoIn={loadDtoIn} />);
+  //   expect(onLoad).toHaveBeenCalledTimes(1);
+  //   expect(onLoad).lastCalledWith(loadDtoIn);
+  //   await wait();
+  //   expect(wrapper.text()).toContain("Hello, world!");
+  // });
+});
